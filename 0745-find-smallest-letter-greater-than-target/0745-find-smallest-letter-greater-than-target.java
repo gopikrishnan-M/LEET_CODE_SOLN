a27ed8331291ab207fa18d334e49cf7f04974659
gopi krishnan M <gopikris0021@gmail.com>
2025-02-26 14:17:36 +0530

    Time: 144 ms (0.8%), Space: 44.1 MB (91.77%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -2,9 +2,6 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int low=0;
         int high=letters.length-1;
-        if(target>=letters[high]){
-            return letters[0];
-        }
         while(low<=high){
             int mid=low+(high-low)/2;
            if(letters[mid]>target)
@@ -12,7 +9,7 @@ class Solution {
             else
                 low=mid+1;
         }
-        return letters[low];
+        return letters[low % letters.length];
         
     }
 }
