746d845f744d619e0b188941bd408ac7b1542b69
gopi krishnan M <gopikris0021@gmail.com>
2025-03-16 11:35:52 +0530

    Time: 0 ms (100%), Space: 44.8 MB (24.94%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -3,9 +3,9 @@ class Solution {
         int temp=0;
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col <= (arr[row].length-1)/2; col++) {
-                temp=arr[row][col];
+                temp=arr[row][col]^1;
                 arr[row][col]=arr[row][arr[row].length-1-col]^1;
-                arr[row][arr[row].length-1-col]=temp^1;
+                arr[row][arr[row].length-1-col]=temp;
             }
         }
         return arr;
