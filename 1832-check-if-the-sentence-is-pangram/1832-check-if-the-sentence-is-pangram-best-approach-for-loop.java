f65badf2fb69819e63c4ee8603f8ef7cf960cefc
gopi krishnan M <gopikris0021@gmail.com>
2025-03-30 10:50:46 +0530

    Delete 1832-check-if-the-sentence-is-pangram directory

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram-best-approach-for-loop.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean checkIfPangram(String s) {
-        HashSet<Character> set =new HashSet<>();
-        for (char c:s.toCharArray()) {
-            set.add(c);
-        }
-        return set.size()==26;
-    }
-}
--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean checkIfPangram(String sentence) {
-        if(sentence.length() < 26) return false;
-        for(int i='a' ; i <= 'z' ; i++){
-            if(sentence.indexOf(i)<0) return false;
-        }
-        return true;
-    }
-}
