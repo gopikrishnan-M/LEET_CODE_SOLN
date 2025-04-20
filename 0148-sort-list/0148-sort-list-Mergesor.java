3b252c2836331d37ae945599b478c85f062ca972
gopi krishnan M <gopikris0021@gmail.com>
2025-04-20 23:22:39 +0530

    Time: 8 ms (99.27%), Space: 57.3 MB (23.73%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list-Mergesor.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+   public ListNode sortList(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode f=head;
+        ListNode s=head;
+        ListNode p=null;
+        while(f!=null&&f.next!=null){
+           p=s;
+           s=s.next;
+           f=f.next.next;
+        }
+        if(p!=null){
+            p.next=null;
+        }
+        ListNode left=sortList(head);
+        ListNode right=sortList(s);
+        return merge(left,right);
+
+    }
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail =dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail. next= list1;
+                list1=list1.next;
+                tail = tail. next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next=(list1!=null)?list1:list2;
+        return dummyHead.next;
+    }
+    
+}
