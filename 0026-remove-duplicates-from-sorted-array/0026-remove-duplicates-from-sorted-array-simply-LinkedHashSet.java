f37ddf967b8d1da41d39f7169de584296217a43d
gopi krishnan M <gopikris0021@gmail.com>
2025-04-22 18:44:07 +0530

    Time: 4 ms (7.7%), Space: 44.9 MB (61.06%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-simply-LinkedHashSet.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> nodup = new LinkedHashSet<>();
+        for(int num:nums){
+            nodup.add(num);
+        }
+        int i=0;
+        for(int num:nodup){
+            nums[i++]=num;
+        }
+        return nodup.size();
+    }
+}
