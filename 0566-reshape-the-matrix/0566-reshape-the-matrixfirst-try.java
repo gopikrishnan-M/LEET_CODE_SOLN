b6ffc2a969d62885947f81cd49c5ab45913d5bdf
gopi krishnan M <gopikris0021@gmail.com>
2025-04-27 12:58:26 +0530

    Time: 1 ms (66.51%), Space: 45.6 MB (26.32%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrixfirst-try.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length, n = mat[0].length;
+        if (m * n != r * c) return mat;
+        int[] temp=new int[m*n];
+        int[][] ans=new int[r][c];
+        int row=0,col=0;
+        int t=0;
+        for(int[] arr:mat){
+            for(int num:arr){
+                temp[t++]=num;
+            }
+        }
+        t=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ans[i][j]=temp[t++];
+            }
+        }
+        return ans;
+    }
+}
