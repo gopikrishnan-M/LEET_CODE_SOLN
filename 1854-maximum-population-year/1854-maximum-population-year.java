e7d2fefa9807bf8fed7b535b8c59f37e111ca6f5
gopi krishnan M <gopikris0021@gmail.com>
2025-05-04 12:51:22 +0530

    Time: 0 ms (100%), Space: 41.9 MB (45.36%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
        int[] papu=new int[101];
+       //can also use a nested for/while loop to icrement all val from start to end
+       //and finally check for the largest element and update the index val
+       //index value plus 1950 will be the answer
        for(int[] person:logs){
         papu[person[0]-1950]++;
         papu[person[1]-1950]--;
