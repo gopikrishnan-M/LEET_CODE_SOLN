b8b1701b94f877327b165b5e90694b836c925198
gopi krishnan M <gopikris0021@gmail.com>
2025-05-10 19:36:53 +0530

    Time: 6 ms (90.58%), Space: 57.6 MB (13.36%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -9,48 +9,44 @@
  * }
  */
 class Solution {
-    public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int[][] ans=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+    public int[][] spiralMatrix(int row, int col, ListNode head) {
+        int[][] ans=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 ans[i][j]=-1;
             }
         }
-        boundary(ans,head,0,m-1,0,n-1);
+        int rs=0,re=row-1,cs=0,ce=col-1;
+        while( rs<=re && cs<=ce &&  head!=null){
+            //top row
+            for(int i=cs;i<=ce&&head!=null;i++){
+                ans[rs][i]=head.val;
+                head=head.next;
+            }
+            rs++;
+            //last col
+            for(int i=rs;i<=re&&head!=null;i++){
+                ans[i][ce]=head.val;
+                head=head.next;
+            }
+            ce--;
+            //last row
+            if (rs <= re) {
+                for(int i=ce;i>=cs&&head!=null;i--){
+                    ans[re][i]=head.val;
+                    head=head.next;
+                }
+                re--;
+            }
+            //move up
+            if(cs<=ce){
+                for(int i=re;i>=rs&&head!=null;i--){
+                    ans[i][cs]=head.val;
+                    head=head.next;
+                }
+                cs++;
+            }
+        }
         return ans;
-    }
-    public static void boundary(int[][] ans,ListNode head,int rs,int re,int cs,int ce){
-        if(rs>re||cs>ce||head==null){//basic just do it
-            return;
-        }
-        //top row
-        for(int i=cs;i<=ce&&head!=null;i++){
-            ans[rs][i]=head.val;
-            head=head.next;
-        }
-        rs++;
-        //last col
-        for(int i=rs;i<=re&&head!=null;i++){
-            ans[i][ce]=head.val;
-            head=head.next;
-        }
-        ce--;
-        //last row
-        if (rs <= re) {
-            for(int i=ce;i>=cs&&head!=null;i--){
-                ans[re][i]=head.val;
-                head=head.next;
-            }
-            re--;
-        }
-        //move up
-        if(cs<=ce){
-            for(int i=re;i>=rs&&head!=null;i--){
-                ans[i][cs]=head.val;
-                head=head.next;
-            }
-            cs++;
-        }
-        boundary(ans, head, rs, re, cs, ce);
     }
 }
