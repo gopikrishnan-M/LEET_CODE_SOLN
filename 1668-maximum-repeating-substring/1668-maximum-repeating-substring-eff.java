e022302cd8498c98efe786967dc4278d35c3b0fa
gopi krishnan M <gopikris0021@gmail.com>
2025-05-16 10:14:20 +0530

    Time: 0 ms (100%), Space: 41.8 MB (44.49%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring-eff.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int count=1;
+        while(sequence.contains(word.repeat(count))){
+            ++count;
+        }
+        return count-1;
+    }
+}
