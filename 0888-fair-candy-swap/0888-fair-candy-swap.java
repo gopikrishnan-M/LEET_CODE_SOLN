3a8308fac0a9de9882920b46394cd409e864272f
gopi krishnan M <gopikris0021@gmail.com>
2025-05-21 22:38:11 +0530

    Time: 618 ms (5.14%), Space: 45.7 MB (84.73%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -3,11 +3,15 @@ class Solution {
         int ac=0,bc=0,al=ace.length,bl=bob.length;
         for(int n:ace) ac+=n;        
         for(int m:bob) bc+=m;
-        for(int i=0;i<al;i++){
-            for(int j=0;j<bl;j++){
-                if((ac-ace[i]+bob[j])==(bc-bob[j]+ace[i])) return new int[] {ace[i],bob[j]};
+        int i=0,j=0;
+        while(i<al){
+            if(j>=bl){
+                j=0;
+                i++;
             }
-        } 
-        return new int[]{0,0};       
+            if(bob[j]==ace[i]+(bc-ac)/2) return new int[] {ace[i],bob[j]};
+            else j++;
+        }
+        return new int[]{0,0};
     }
 }
