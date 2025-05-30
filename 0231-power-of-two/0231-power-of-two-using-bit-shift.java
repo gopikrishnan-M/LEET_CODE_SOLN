1a356c86a9e5535dcb2ebb95884b0f7078be6f25
gopi krishnan M <gopikris0021@gmail.com>
2025-05-30 17:42:56 +0530

    Time: 1 ms (36.8%), Space: 40.4 MB (98.33%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two-using-bit-shift.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0) return false;
+        int s=0,e=30;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            long val=1L << mid;
+            if(val==n) return true;
+            else if(val<n) s=mid+1;
+            else e=mid-1;
+        }
+        return false;
+    }
+}
