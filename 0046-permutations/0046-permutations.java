28f6c57fdd6ce163c2804cd702de0e7705353312
gopi krishnan M <gopikris0021@gmail.com>
2025-07-08 09:01:29 +0530

    Time: 2 ms (37.66%), Space: 44.7 MB (29.96%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        return helper(new ArrayList<>(),nums);
-    } 
-    public List<List<Integer>> helper(List<Integer> p, int[] up) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (up.length == 0) {
+        return posibilities(new ArrayList<Integer>(),nums,0);
+    }
+    private static List<List<Integer>> posibilities(List<Integer> p, int[] arr, int track) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(track==arr.length){
             ans.add(new ArrayList<>(p));
             return ans;
         }
-        int first = up[0];
-        int[] rest = Arrays.copyOfRange(up, 1, up.length);
-
-        for (int i = 0; i <= p.size(); i++) {
-            List<Integer> temp = new ArrayList<>(p);
-            temp.add(i, first); // insert at position i
-            ans.addAll(helper(temp, rest));
+        int a=arr[track++];
+        for(int i=0;i<=p.size();i++){
+            p.add(i,a);
+            ans.addAll(posibilities(p,arr,track));
+            p.remove(i);
         }
-
         return ans;
     }
 }
