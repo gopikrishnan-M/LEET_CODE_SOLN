c4d7787c7dfccdda8e25861761cb7867068ef9d5
gopi krishnan M <gopikris0021@gmail.com>
2025-07-18 12:15:12 +0530

    Time: 0 ms (100%), Space: 44.5 MB (44.48%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,42 +1,43 @@
 class Solution {
     public Node connect(Node root) {
-        if(root==null){
+        if (root == null) {
             return root;
         }
-        helper(root);
+        Node leftmost = root;
+        while (leftmost.left != null) {
+            Node current = leftmost;
+            while (current != null) {
+                current.left.next = current.right;
+                if (current.next != null) {
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
+            }
+            leftmost = leftmost.left;
+        }
         return root;
+
     }
-    public void helper(Node node){
-        if(node.left==null){
-            return;
-        }
-        node.left.next=node.right;
-        if(node.next!=null){
-            node.right.next=node.next.left;
-        }
-        helper(node.left);
-        helper(node.right);        
-    }
+
 }/*
-// Definition for a Node.
-class Node {
+ // Definition for a Node.
+ class Node {
     public int val;
     public Node left;
     public Node right;
     public Node next;
-
+ 
     public Node() {}
     
     public Node(int _val) {
         val = _val;
     }
-
+ 
     public Node(int _val, Node _left, Node _right, Node _next) {
         val = _val;
         left = _left;
         right = _right;
         next = _next;
     }
-};
-*/
-
+ };
+ */
