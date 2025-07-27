2d62dd9704075451b5f88a465f052d7da86c86e3
gopi krishnan M <gopikris0021@gmail.com>
2025-07-27 22:06:18 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.15%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -18,9 +18,11 @@ class Solution {
         if(root==null){
             return false;
         }
+        //we must check weather the sum is equal or not till the root but not in between it should form a complete path from root to leaf so we check equal to target , left and right should be null
         if(root.val==targetSum && root.left==null && root.right==null){
             return true;
         }
+    //here subtract the current value from the root value and pass that value to the next sucessor recursion when the condition is satisfied we get true as there is "||" operator in between
         return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
 }
